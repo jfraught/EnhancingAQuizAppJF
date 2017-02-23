310648f00bea20786f94a1231ec939bb87f6c33d
Jordan Fraughton <jordan.fraughoton@gmail.com>
2017-02-23 14:17:21 -0700

    Added class of Trivia.

---
+++ TrueFalseStarter/Trivia.swift
@@ -0,0 +1,21 @@
+//
+//  Trivia.swift
+//  TrueFalseStarter
+//
+//  Created by Jordan Fraughton on 2/23/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+
+class Trivia {
+   
+    let question: String
+    let answer: String 
+
+    init(question: String, answer: String) {
+        self.question = question
+        self.answer = answer
+    }
+}
+
